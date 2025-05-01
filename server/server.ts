93592c34e288b6616f4c369ee0f5c1749488b7eb
Hugo Rodriguez <hugorogz@Hugos-MacBook-Pro.local>
2025-05-01 01:16:00 -0600

    removed log from server

--- server/server.ts
+++ server/server.ts
@@ -152,7 +152,6 @@ app.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
 
-        console.log(users)
         res.json(users);
     } catch (err) {
         console.error(err);
