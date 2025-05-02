1b9f73afc272c66604464606eab5dde06e9c9f59
Hugo Rodriguez <hugorogz@Hugos-MacBook-Pro.local>
2025-05-02 14:36:01 -0600

    changes for consistency

--- server/seed.js
+++ server/seed.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { MongoClient } from 'mongodb';
 
-// Conexión a la base de datos MongoDB
+// Connection to mongo DB
 const url = 'mongodb://localhost:27017';
 const dbName = 'my-notes-app';
 const client = new MongoClient(url);
@@ -26,8 +26,8 @@ const testNotes = [
     description: "<p><strong>Lorem</strong> ipsum dolor sit amet, <em>consectetur</em> adipiscing elit.</p>",
     created_at: "2025-04-28T22:26:41.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [`${users[1].id}`] // incluimos a UserA en userAccess, lo que significa que esta es una nota compartida de UserA con UserB
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [`${users[1].id}`] // We include UserB in userAccess, this means that is a note shared fro UserA to UserB
   },
   {
     id: uuidv4(),
@@ -35,8 +35,8 @@ const testNotes = [
     description: "<p>Sed do eiusmod <u>tempor incididunt</u> ut labore et dolore magna aliqua.</p>",
     created_at: "2025-04-28T21:15:30.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [`${users[1].id}`] // incluimos a UserA en userAccess, lo que significa que esta es una nota compartida de UserA con UserB
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [`${users[1].id}`] // We include UserB in userAccess, this means that is a note shared fro UserA to UserB
   },
   {
     id: uuidv4(),
@@ -44,8 +44,8 @@ const testNotes = [
     description: "<p>Ut enim ad <strong>minim veniam</strong>, quis nostrud exercitation ullamco.</p>",
     created_at: "2025-04-28T20:03:15.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -53,8 +53,8 @@ const testNotes = [
     description: "<p>Laboris nisi ut aliquip <span style='color: blue;'>ex ea commodo</span> consequat.</p>",
     created_at: "2025-04-28T18:48:22.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -62,8 +62,8 @@ const testNotes = [
     description: "<p>Duis aute irure dolor in <em>reprehenderit</em> in voluptate velit esse cillum.</p>",
     created_at: "2025-04-28T17:37:11.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -71,8 +71,8 @@ const testNotes = [
     description: "<p>Fugiat nulla pariatur. Excepteur <u>sint occaecat</u> cupidatat non proident.</p>",
     created_at: "2025-04-28T16:24:05.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -80,8 +80,8 @@ const testNotes = [
     description: "<p>Sunt in culpa qui officia <strong>deserunt mollit</strong> anim id est laborum.</p>",
     created_at: "2025-04-28T15:10:50.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note 
   },
   {
     id: uuidv4(),
@@ -89,7 +89,7 @@ const testNotes = [
     description: "<p>Curabitur non nulla sit amet <em>nisl tempus</em> convallis quis ac lectus.</p>",
     created_at: "2025-04-28T14:02:31.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
+    userId: users[0].id, // Associate note with UserA
     userAccess: [] // Esta es una nota NO compartida
   },
   {
@@ -98,8 +98,8 @@ const testNotes = [
     description: "<p>Vivamus magna justo, lacinia eget <span style='color: green;'>consectetur sed</span>, convallis at tellus.</p>",
     created_at: "2025-04-28T12:48:19.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, /// Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -107,18 +107,17 @@ const testNotes = [
     description: "<p>Donec rutrum congue leo eget <strong>malesuada</strong>.</p>",
     created_at: "2025-04-28T11:32:44.236Z",
     updated_at: null,
-    userId: users[0].id, // Asociamos la nota con UserA
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[0].id, // Associate note with UserA
+    userAccess: [] // This is NOT a shared note
   },
-  // Notas de UserB
   {
     id: uuidv4(),
     title: "Nota 1 de UserB",
     description: "<p>Descripción de la nota de <strong>UserB</strong></p>",
     created_at: "2025-04-28T09:00:00.000Z",
     updated_at: null,
-    userId: users[1].id, // Asociamos la nota con UserB
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[1].id, // Associate note with UserB
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -126,8 +125,8 @@ const testNotes = [
     description: "<p>Otra nota de <strong>UserB</strong></p>",
     created_at: "2025-04-28T08:15:00.000Z",
     updated_at: null,
-    userId: users[1].id, // Asociamos la nota con UserB
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[1].id, // Associate note with UserB
+    userAccess: [] // This is NOT a shared note
   },
   {
     id: uuidv4(),
@@ -135,8 +134,8 @@ const testNotes = [
     description: "<p>Descripción final de la nota de <strong>UserB</strong></p>",
     created_at: "2025-04-28T07:45:00.000Z",
     updated_at: null,
-    userId: users[1].id, // Asociamos la nota con UserB
-    userAccess: [] // Esta es una nota NO compartida
+    userId: users[1].id, // Associate note with UserB
+    userAccess: [] // This is NOT a shared note
   }
 ];
 
@@ -147,17 +146,17 @@ async function insertUsersAndNotes() {
 
     const db = client.db(dbName);
 
-    // Insertamos los usuarios
+    // Insert users
     const usersCollection = db.collection('users');
     await usersCollection.insertMany(users);
 
-    // Insertamos las notas asociadas a cada usuario
+    // Insert notes associated with each user
     const notesCollection = db.collection('notes');
     await notesCollection.insertMany(testNotes);
 
-    console.log('Usuarios y notas insertados exitosamente!');
+    console.log('Users and notes successfully inserted!');
   } catch (error) {
-    console.error('Error al insertar usuarios o notas:', error);
+    console.error('Error when tying to insert users and notes:', error);
   } finally {
     await client.close();
   }
